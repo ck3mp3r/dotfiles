f7622f7feaec22fb84f8d5139220b1f5229918f6
Christian Kemper <christian.kemper@me.com>
2025-09-21 18:24:20 +0100

    feat(atuin): update init script and filter mode
    - Add --disable-up-arrow flag to atuin init nu command in init script
    - Change filter_mode setting from host to global in configuration

--- modules/programs/atuin/default.nix
+++ modules/programs/atuin/default.nix
@@ -2,7 +2,7 @@
   atuinConfig = pkgs.runCommand "atuin-init-nu" {buildInputs = [pkgs.gnused];} ''
     export HOME=$TMPDIR
     mkdir -p $out
-    ${pkgs.atuin}/bin/atuin init nu > $out/init.nu
+    ${pkgs.atuin}/bin/atuin init nu --disable-up-arrow > $out/init.nu
     sed -i 's/get -i/get -o/g' $out/init.nu
   '';
 in {
@@ -11,7 +11,7 @@ in {
     enableNushellIntegration = false;
     flags = ["--disable-up-arrow"];
     settings = {
-      filter_mode = "host";
+      filter_mode = "global";
       enter_accept = false;
       keymap_mode = "vim-normal";
       history_filter = [
